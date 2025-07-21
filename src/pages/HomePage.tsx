e52931ae3179807585f91c11d1b9613c955c28e8
Alex Wilder <alexwilder.dev@gmail.com>
2025-07-21 16:25:23 +0100

    Remove unused Link import

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import VenueCard from '../components/VenueCard';
 import SeasonalCarousel from '../components/SeasonalCarousel';
